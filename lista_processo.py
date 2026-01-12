143a13df49f21c32109454bf04b23fbee9a15182
Nicolas Inacio Martins Mesquita <46948807@PMSBC.GOV.BR>
2026-01-12 12:29:28 -0300

    chore: ajustes nas funcoes

--- lista_processo.py
+++ lista_processo.py
@@ -1,19 +1,24 @@
+
 import logging
 import psutil
 import sys
 import time
 import os
 
-processo = 'Evernote.exe'
 lista_processo = {}
 
 def main():
     limpa_tela()
+    processo = escolher_programa()
     config_loggin()
-    listar_processo()
-    validar_processo()
+    listar_processo(processo)
+    validar_processo(processo)  
 
-def listar_processo():
+def escolher_programa():
+    processo = input('Digite o nome do processo: ')
+    return processo
+
+def listar_processo(processo):
     ram = 0
 
     for x in psutil.process_iter(['pid','name']):
@@ -35,7 +40,7 @@ def listar_processo():
             return lista_processo
             break
 
-def consultar_processo_ativo():
+def consultar_processo_ativo(processo):
     logger = logging.getLogger('monitoramento_processos')
     trava_loop = True
     try:
@@ -44,19 +49,19 @@ def consultar_processo_ativo():
             if processo in lista_processo.get('proc', {}).get('name',{}):
                 logger.info(f'O processo {processo} continua ativo.')
                 time.sleep(10)
-                listar_processo() 
+                listar_processo(processo) 
             else:
                 logger.info(f'O processo {processo} foi finalizado. Validando novamente...')
                 time.sleep(10)
-                listar_processo()
+                listar_processo(processo)
                 trava_loop = False
-                validar_processo()
+                validar_processo(processo)
 
     except KeyboardInterrupt:
         logger.warning('O usuário interrompeu o programa (CRTL+C).')
         sys.exit(0)
 
-def validar_processo():
+def validar_processo(processo):
     logger = logging.getLogger('monitoramento_processos')
     trava_loop = True
     try:
@@ -66,23 +71,29 @@ def validar_processo():
                 logger.info(f'O processo {processo} está ativo no PID {lista_processo['proc']['pid']} com o consumo de {lista_processo['proc']['ram']} Mb de RAM.')
                 trava_loop = False
                 time.sleep(10)
-                consultar_processo_ativo()
+                consultar_processo_ativo(processo)
             
             else:
                 logger.info(f'O processo {processo} não foi encontrado. Procurando novamente em 10 segundos.')
                 time.sleep(10)
-                listar_processo()
+                listar_processo(processo)
     
     except KeyboardInterrupt:
         logger.warning('O usuário interrompeu o programa (CRTL+C).')
         sys.exit(0)
 
-def limpa_tela():
-    return os.system('cls') if os.name == 'nt' else os.system('clear')
-
 def config_loggin():
     logger = logging.getLogger('monitoramento_processos')
     logger.setLevel(logging.INFO)
+
+    logging.basicConfig(
+        filename='app.log',
+        filemode='a',
+        format='[%(levelname)s] - %(asctime)s - %(message)s',
+        datefmt='%d/%m/%Y %H:%M:%S',
+        level=logging.INFO,
+        encoding='utf-8'
+    )
 
     handler = logging.StreamHandler()
     handler.setLevel(logging.INFO)
@@ -92,5 +103,8 @@ def config_loggin():
     handler.setFormatter(formatter)
 
     logger.addHandler(handler)
-    
+
+def limpa_tela():
+    return os.system('cls') if os.name == 'nt' else os.system('clear')
+
 main()
